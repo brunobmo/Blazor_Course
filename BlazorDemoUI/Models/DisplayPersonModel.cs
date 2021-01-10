37451e62e652bb34cfc4e76a7e56dee81c3c78e9
bmo <bruno.oliveira360@gmail.com>
2021-01-10 22:26:49 +0000

    Examples

---
+++ BlazorDemoUI/Models/DisplayPersonModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoUI.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First Name is too long.")]
+        [MinLength(5, ErrorMessage = "First Name is too short.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last Name is too long.")]
+        [MinLength(5, ErrorMessage = "Last Name is too short.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ TodoList/DataAcessLibrary/CompanyData.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class CompanyData : ICompanyData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public CompanyData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<CompanyModel>> GetCompany()
+        {
+            string sql = "select * from dbo.People";
+
+            return _db.LoadData<CompanyModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertCompany(CompanyModel company)
+        {
+            string sql = @"insert into dbo.People (FirstName, LastName, EmailAddress)
+                           values (@FirstName, @LastName, @EmailAddress);";
+
+            return _db.SaveData(sql, company);
+        }
+    }
+}
