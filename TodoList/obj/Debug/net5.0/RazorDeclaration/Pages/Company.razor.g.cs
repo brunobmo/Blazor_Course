7b0404445ebcd7861897b7f339f7c2b3d0a02bab
bmo <bruno.oliveira360@gmail.com>
2021-01-10 22:53:35 +0000

    update

--- TodoList/obj/Debug/net5.0/RazorDeclaration/Pages/Company.razor.g.cs
+++ TodoList/obj/Debug/net5.0/RazorDeclaration/Pages/Company.razor.g.cs
@@ -13,91 +13,91 @@ namespace TodoList.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 1 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 2 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 3 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 4 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 5 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 6 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 7 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 8 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 9 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using TodoList;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 10 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using TodoList.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\Pages\Company.razor"
+#line 3 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\Pages\Company.razor"
 using DataAccessLibrary;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\Pages\Company.razor"
+#line 4 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\Pages\Company.razor"
 using DataAccessLibrary.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\Pages\Company.razor"
+#line 5 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\Pages\Company.razor"
 using TodoList.Models;
 
 #line default
@@ -112,7 +112,7 @@ using TodoList.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 22 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\Pages\Company.razor"
+#line 22 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\Pages\Company.razor"
        
     private List<CompanyModel> company;
     private DisplayCompanyModel newCompany = new DisplayCompanyModel();
--- TodoList/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ TodoList/obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace TodoList.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 1 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 2 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 3 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 4 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 5 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 6 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 7 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 8 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 9 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using TodoList;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\_Imports.razor"
+#line 10 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\_Imports.razor"
 using TodoList.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using TodoList.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\bruno\OneDrive\Documentos\Projetos\TodoList\Pages\Counter.razor"
+#line 9 "c:\Users\bruno\OneDrive\Documentos\Projetos\BlazorRepo\Blazor_Course\TodoList\Pages\Counter.razor"
        
     private int currentCount = 0;
 
