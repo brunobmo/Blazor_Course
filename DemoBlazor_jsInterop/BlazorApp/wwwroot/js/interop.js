b5c455941081028bfe978e6cf7fb4c35e36fcfad
Bruno <bruno.oliveira360@gmail.com>
2021-05-13 19:43:13 +0100

    Demo Blazor: JS Interop

---
+++ DemoBlazor_jsInterop/BlazorApp/wwwroot/js/interop.js
@@ -0,0 +1,10 @@
+//object created to support windows events
+window.loadEvent = {
+    play: () => {
+        testeAlert();
+    } 
+ };
+ function testeAlert(){
+    alert("This is a new Alert");
+}
+
